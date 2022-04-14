8202acc62b0363a0f5e9e933e721dcde9cdacbe5
Kien Nguyen <kiennt2609@gmail.com>
2022-04-14 14:34:53 +0700

    Update

--- starred/starred.py
+++ starred/starred.py
@@ -22,7 +22,6 @@ desc = '''# Awesome Stars [![Awesome](https://cdn.rawgit.com/sindresorhus/awesom
 
 license_ = '''
 ## License
-
 [![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)]\
 (https://creativecommons.org/publicdomain/zero/1.0/)
 
